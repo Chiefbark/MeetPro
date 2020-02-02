402fa8198039f3b27835bb8f98a0c292d0c03b3c
Chiefbark <chiefbark.dev@gmail.com>
2020-02-02 15:17:32 +0100

    Added DAOUser class

---
+++ app/src/main/java/com/example/meetpro/dao/DAOUser.java
@@ -0,0 +1,53 @@
+package com.example.meetpro.dao;
+
+import com.example.meetpro.model.User;
+import com.example.meetpro.model.UserList;
+
+public class DAOUser {
+
+    /**
+     * Inserts a new User into the database
+     *
+     * @param user The User to insert
+     */
+    public static void insert(User user) {
+
+    }
+
+    /**
+     * Selects all the Users from the database
+     *
+     * @return The UserList of Users from the database
+     */
+    public static UserList select() {
+        return null;
+    }
+
+    /**
+     * Selects the User with the specific id from the database
+     *
+     * @param id The id of the User to select
+     * @return The User selected
+     */
+    public static User select(int id) {
+        return null;
+    }
+
+    /**
+     * Updates an User into the database
+     *
+     * @param user The User to update
+     */
+    public static void update(User user) {
+
+    }
+
+    /**
+     * Deletes an User from the database
+     *
+     * @param id The id of the User to delete
+     */
+    public static void delete(int id) {
+
+    }
+}
