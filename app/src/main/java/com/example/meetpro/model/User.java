fa06312645518fb28a83638f05bd2303a30dde9a
Chiefbark <chiefbark.dev@gmail.com>
2020-02-02 13:56:26 +0100

    Created user model

---
+++ app/src/main/java/com/example/meetpro/model/User.java
@@ -0,0 +1,132 @@
+package com.example.meetpro.model;
+
+public class User {
+
+    private String name;
+    private String surname;
+    private int phone;
+    private String mail;
+    private String password;
+
+    /**
+     * Constructor of the User
+     *
+     * @param name    The name of the User
+     * @param surname The surname of the User
+     * @param phone   The phone of the User
+     * @param mail    The mail of the User
+     */
+    public User(String name, String surname, int phone, String mail) {
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+        this.mail = mail;
+    }
+
+    /**
+     * Constructor og the User
+     *
+     * @param name     The name of the User
+     * @param surname  The surname of the User
+     * @param phone    The phone of the User
+     * @param mail     The mail of the User
+     * @param password The password of the User
+     */
+    public User(String name, String surname, int phone, String mail, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+        this.mail = mail;
+        this.password = password;
+    }
+
+    /**
+     * Returns the name of the User
+     *
+     * @return The name of the User
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the name to the User
+     *
+     * @param name The new name of the User
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns the surname of the User
+     *
+     * @return The surname of the User
+     */
+    public String getSurname() {
+        return surname;
+    }
+
+    /**
+     * Sets the surname to the User
+     *
+     * @param surname The new surname of the User
+     */
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    /**
+     * Returns the phone of the User
+     *
+     * @return The phone of the User
+     */
+    public int getPhone() {
+        return phone;
+    }
+
+    /**
+     * Sets the phone of the User
+     *
+     * @param phone The new phone of the User
+     */
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * Returns the mail of the User
+     *
+     * @return The mail of the User
+     */
+    public String getMail() {
+        return mail;
+    }
+
+    /**
+     * Sets the mail of the User
+     *
+     * @param mail The new mail of the User
+     */
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    /**
+     * Returns the password of the User
+     *
+     * @return The password of the User
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Sets the password of the User
+     *
+     * @param password The new password of the User
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
