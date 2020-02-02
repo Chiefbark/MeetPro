a0fefa48d5bf25eadf1b04215ec14dc91fdcfeec
Chiefbark <chiefbark.dev@gmail.com>
2020-02-02 15:16:55 +0100

    Added id field

--- app/src/main/java/com/example/meetpro/model/User.java
+++ app/src/main/java/com/example/meetpro/model/User.java
@@ -2,11 +2,48 @@ package com.example.meetpro.model;
 
 public class User {
 
+    private int id;
     private String name;
     private String surname;
     private int phone;
     private String mail;
     private String password;
+
+    /**
+     * Constructor of the User
+     *
+     * @param id      The id of the User
+     * @param name    The name of the User
+     * @param surname The surname of the User
+     * @param phone   The phone of the User
+     * @param mail    The mail of the User
+     */
+    public User(int id, String name, String surname, int phone, String mail) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+        this.mail = mail;
+    }
+
+    /**
+     * Constructor og the User
+     *
+     * @param id       The id of the User
+     * @param name     The name of the User
+     * @param surname  The surname of the User
+     * @param phone    The phone of the User
+     * @param mail     The mail of the User
+     * @param password The password of the User
+     */
+    public User(int id, String name, String surname, int phone, String mail, String password) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+        this.mail = mail;
+        this.password = password;
+    }
 
     /**
      * Constructor of the User
@@ -38,6 +75,24 @@ public class User {
         this.phone = phone;
         this.mail = mail;
         this.password = password;
+    }
+
+    /**
+     * Returns the id of the User
+     *
+     * @return The id of the User
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets the id of the User
+     *
+     * @param id The new id of the User
+     */
+    public void setId(int id) {
+        this.id = id;
     }
 
     /**
