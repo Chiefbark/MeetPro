d43498de9f12ee6abaa15cec462c708fc29da1d7
Chiefbark <chiefbark.dev@gmail.com>
2020-02-04 22:37:37 +0100

    Added DAOSkill class

---
+++ app/src/main/java/com/example/meetpro/dao/DAOSkill.java
@@ -0,0 +1,53 @@
+package com.example.meetpro.dao;
+
+import com.example.meetpro.model.Skill;
+import com.example.meetpro.model.SkillList;
+
+public class DAOSkill {
+
+    /**
+     * Inserts a new Skill into the database
+     *
+     * @param skill The Skill to insert
+     */
+    public static void insert(Skill skill) {
+
+    }
+
+    /**
+     * Selects all the Skills from the database
+     *
+     * @return The SkillList of Skills from the database
+     */
+    public static SkillList select() {
+        return null;
+    }
+
+    /**
+     * Selects the Skill with the specific id from the database
+     *
+     * @param id The id of the Skill to select
+     * @return The Skill selected
+     */
+    public static Skill select(int id) {
+        return null;
+    }
+
+    /**
+     * Updates an Skill into the database
+     *
+     * @param skill The Skill to update
+     */
+    public static void update(Skill skill) {
+
+    }
+
+    /**
+     * Deletes an Skill from the database
+     *
+     * @param id The id of the Skill to delete
+     */
+    public static void delete(int id) {
+
+    }
+}
