ac9124430a4b9f50176bff7002184973bba82c9d
Chiefbark <chiefbark.dev@gmail.com>
2020-02-05 13:23:14 +0100

    profileSelf renamed and layoout modified

--- app/src/main/java/com/example/meetpro/ProfileSelf.java
+++ app/src/main/java/com/example/meetpro/ProfileSelf.java
@@ -1,15 +1,12 @@
 package com.example.meetpro;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.LayoutInflater;
 
 public class ProfileSelf extends Template {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.addContent(R.layout.activity_profile_self);
+        this.addContent(R.layout.profile_self);
     }
 }
